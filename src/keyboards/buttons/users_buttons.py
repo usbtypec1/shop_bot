e08c39934be53e8c41e7ef111f99243745ef1a69
Eldos <eldos.baktybekov@gmail.com>
2023-05-18 19:46:06 +0600

    :recycle: Refactor user responses

--- src/keyboards/buttons/users_buttons.py
+++ src/keyboards/buttons/users_buttons.py
@@ -6,15 +6,24 @@ from keyboards.inline import callback_factories
 
 
 class UserButton(aiogram.types.InlineKeyboardButton):
-    def __init__(self, user_id: int, user_tg_id: int, balance: float,
-                 registration_date: datetime.date, username: str = None, **callback_data):
+    def __init__(
+            self,
+            user_id: int,
+            user_tg_id: int,
+            balance: float,
+            registration_date:
+            datetime.date,
+            username: str = None,
+            **callback_data,
+    ):
         callback_data.pop('@')
         callback_data['action'] = 'manage'
         callback_data['id'] = user_id
         super().__init__(
             text=f'#{user_tg_id} | {username + " | " if username is not None else ""}'
                  f'${balance} | {registration_date:%m/%d/%Y}',
-            callback_data=callback_factories.UserCallbackFactory().new(**callback_data)
+            callback_data=callback_factories.UserCallbackFactory().new(
+                **callback_data)
         )
 
 
@@ -22,9 +31,10 @@ class SearchUsersButton(aiogram.types.InlineKeyboardButton):
     def __init__(self, **callback_data):
         callback_data.pop('@')
         callback_data['action'] = 'search'
-        super().__init__('🔎 Search Users', callback_data=callback_factories.UserCallbackFactory().new(
-            **callback_data
-        ))
+        super().__init__('🔎 Search Users',
+                         callback_data=callback_factories.UserCallbackFactory().new(
+                             **callback_data
+                         ))
 
 
 class BanUserButton(aiogram.types.InlineKeyboardButton):
@@ -34,7 +44,8 @@ class BanUserButton(aiogram.types.InlineKeyboardButton):
         callback_data['id'] = str(user_id)
         super().__init__(
             text='📛 Ban',
-            callback_data=callback_factories.UserCallbackFactory().new(**callback_data)
+            callback_data=callback_factories.UserCallbackFactory().new(
+                **callback_data)
         )
 
 
@@ -45,7 +56,8 @@ class UnbanUserButton(aiogram.types.InlineKeyboardButton):
         callback_data['id'] = str(user_id)
         super().__init__(
             text='🆓 Unban',
-            callback_data=callback_factories.UserCallbackFactory().new(**callback_data)
+            callback_data=callback_factories.UserCallbackFactory().new(
+                **callback_data)
         )
 
 
@@ -54,7 +66,8 @@ class TopUpBalanceButton(aiogram.types.InlineKeyboardButton):
         super().__init__(
             text='💸 Top Up Balance',
             callback_data=callback_factories.TopUpUserBalanceCallbackFactory().new(
-                user_id=user_id, balance_delta='', payment_method='', is_confirmed=''
+                user_id=user_id, balance_delta='', payment_method='',
+                is_confirmed=''
             )
         )
 
@@ -76,7 +89,8 @@ class DeleteUserButton(aiogram.types.InlineKeyboardButton):
         callback_data['id'] = str(user_id)
         super().__init__(
             text='🫥 Delete User',
-            callback_data=callback_factories.UserCallbackFactory().new(**callback_data)
+            callback_data=callback_factories.UserCallbackFactory().new(
+                **callback_data)
         )
 
 
@@ -85,7 +99,8 @@ class P2PDeliveryButton(aiogram.types.InlineKeyboardButton):
         super().__init__(
             text='🤝 P2P Delivery',
             callback_data=callback_factories.EditUserBalanceCallbackFactory().new(
-                user_id, balance=balance, reason='p2p_delivery', is_confirmed='yes'
+                user_id, balance=balance, reason='p2p_delivery',
+                is_confirmed='yes'
             )
         )
 
@@ -95,7 +110,8 @@ class AdminMistakeButton(aiogram.types.InlineKeyboardButton):
         super().__init__(
             '🫤 Admin Mistake',
             callback_data=callback_factories.EditUserBalanceCallbackFactory().new(
-                user_id, balance=balance, reason='admin_mistake', is_confirmed='yes'
+                user_id, balance=balance, reason='admin_mistake',
+                is_confirmed='yes'
             )
         )
 
@@ -105,7 +121,8 @@ class RefundedPaymentButton(aiogram.types.InlineKeyboardButton):
         super().__init__(
             '🔄 Refunded Payment',
             callback_data=callback_factories.EditUserBalanceCallbackFactory().new(
-                user_id, balance=balance, reason='refunded_payment', is_confirmed='yes'
+                user_id, balance=balance, reason='refunded_payment',
+                is_confirmed='yes'
             )
         )
 
@@ -115,7 +132,8 @@ class CashAppPaymentMethod(aiogram.types.InlineKeyboardButton):
         super().__init__(
             '💳 Cashapp',
             callback_data=callback_factories.TopUpUserBalanceCallbackFactory().new(
-                user_id, balance_delta=balance_delta, payment_method='cashapp', is_confirmed='yes'
+                user_id, balance_delta=balance_delta, payment_method='cashapp',
+                is_confirmed='yes'
             )
         )
 
@@ -125,6 +143,7 @@ class AnotherPaymentMethod(aiogram.types.InlineKeyboardButton):
         super().__init__(
             '💎 Other',
             callback_data=callback_factories.TopUpUserBalanceCallbackFactory().new(
-                user_id, balance_delta=balance_delta, payment_method='other', is_confirmed='yes'
+                user_id, balance_delta=balance_delta, payment_method='other',
+                is_confirmed='yes'
             )
         )
--- src/responses/users.py
+++ src/responses/users.py
@@ -1,65 +1,110 @@
-import datetime
+from aiogram.types import Message, CallbackQuery, ParseMode
 
-import aiogram.types
-
-from keyboards.inline import users_keyboard, common_keybords, callback_factories
-from responses import base
+from keyboards.inline import (
+    users_keyboard,
+    common_keybords,
+)
+from keyboards.inline.callback_factories import (
+    UserCallbackFactory,
+    EditUserBalanceCallbackFactory,
+    TopUpUserBalanceCallbackFactory
+)
+from responses.base import BaseResponse
 from services.db_api import schemas
+from services.time_utils import get_now_datetime
 
 
-class UsersResponse(base.BaseResponse):
-    def __init__(self, update: aiogram.types.CallbackQuery | aiogram.types.Message,
-                 users: list[schemas.User], total_balance: float, users_filter: str = '',
-                 page: int = 0, page_size: int = 10, callback_data: dict[str, str] = None):
+class UsersResponse(BaseResponse):
+    def __init__(
+            self,
+            update: CallbackQuery | Message,
+            users: list[schemas.User],
+            total_balance: float,
+            users_filter: str = '',
+            page: int = 0,
+            page_size: int = 10,
+            callback_data: dict[str, str] | None = None,
+    ):
         self.__update = update
-        self.__keyboard = users_keyboard.UsersKeyboard(users, page, page_size, users_filter, callback_data)
+        self.__keyboard = users_keyboard.UsersKeyboard(
+            users=users,
+            page=page,
+            page_size=page_size,
+            users_filter=users_filter,
+            callback_data=callback_data,
+        )
         self.__users_quantity = len(users)
         self.__total_balance = total_balance
         self.__page = page
         self.__page_size = page_size
 
     async def _send_response(self):
+        total_balance = self.__total_balance or 0.0
         text = (
             f"Total Users: {self.__users_quantity}\n"
-            f"Total Balances in User's profiles: ${self.__total_balance or 0.0}"
+            f"Total Balances in User's profiles: ${total_balance}"
         )
-        if isinstance(self.__update, aiogram.types.CallbackQuery):
-            await self.__update.answer()
-            await self.__update.message.edit_text(text, reply_markup=self.__keyboard)
-        elif isinstance(self.__update, aiogram.types.Message):
-            await self.__update.answer(text, reply_markup=self.__keyboard)
+        match self.__update:
+            case CallbackQuery():
+                await self.__update.answer()
+                await self.__update.message.edit_text(
+                    text=text,
+                    reply_markup=self.__keyboard,
+                )
+            case Message():
+                await self.__update.answer(text, reply_markup=self.__keyboard)
 
 
-class UserResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User,
-                 number_of_orders: int, callback_data: dict[str, str] = None):
+class UserResponse(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            user: schemas.User,
+            number_of_orders: int,
+            callback_data: dict[str, str] | None = None,
+    ):
         self.__query = query
         self.__user = user
         self.__number_of_orders = number_of_orders
         if callback_data is None:
             callback_data = {
-                '@': 'users', 'filter': '', 'page': '0',
-                'id': user.id, 'action': 'manage', 'is_confirmed': ''
+                '@': 'users',
+                'filter': '',
+                'page': '0',
+                'id': user.id,
+                'action': 'manage',
+                'is_confirmed': '',
             }
         else:
             callback_data['is_confirmed'] = ''
-        self.__keyboard = users_keyboard.UserKeyboard(user.id, user.is_banned, callback_data=callback_data)
+        self.__keyboard = users_keyboard.UserKeyboard(
+            user_id=user.id,
+            is_user_banned=user.is_banned,
+            callback_data=callback_data,
+        )
 
     async def _send_response(self):
+        username = self.__user.username or ''
+        banned_status = 'banned' if self.__user.is_banned else 'not banned'
+        registered_at = f'{self.__user.created_at:%m/%d/%Y}'
         text = (
-                f'<b>User ID</b>: {self.__user.telegram_id}\n' +
-                (f'<b>Username</b>: @{self.__user.username}\n' if self.__user.username else '') +
-                f'<b>Registration Date</b>: {self.__user.created_at.date()}\n'
+                f'<b>User ID</b>: {self.__user.telegram_id}\n'
+                f'<b>Username</b>: @{username}\n'
+                f'<b>Registration Date</b>: {registered_at}\n'
                 f'<b>Number of orders</b>: {self.__number_of_orders}\n'
                 f'<b>Balance</b>: ${self.__user.balance}\n\n'
-                f'<b>Status</b>: {"banned" if self.__user.is_banned else "not banned"}'
+                f'<b>Status</b>: {banned_status}'
         )
         await self.__query.answer()
-        await self.__query.message.edit_text(text, reply_markup=self.__keyboard, parse_mode='HTML')
+        await self.__query.message.edit_text(
+            text=text,
+            reply_markup=self.__keyboard,
+            parse_mode=ParseMode.HTML,
+        )
 
 
-class SearchUserResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery):
+class SearchUserResponse(BaseResponse):
+    def __init__(self, query: CallbackQuery):
         self.__query = query
 
     async def _send_response(self):
@@ -68,20 +113,25 @@ class SearchUserResponse(base.BaseResponse):
         await self.__query.message.edit_text(text)
 
 
-class FoundUsersResponse(base.BaseResponse):
-    def __init__(self, message: aiogram.types.Message):
+class FoundUsersResponse(BaseResponse):
+    def __init__(self, message: Message):
         self.__message = message
 
-    async def _send_response(self) -> aiogram.types.Message:
-        return await self.__message.answer(f'🔡 Found users with these usernames and ids: {self.__message.text}')
+    async def _send_response(self) -> Message:
+        return await self.__message.answer(
+            f'🔡 Found users with these usernames'
+            f' and ids: {self.__message.text}'
+        )
 
 
-class BanUserAlertResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User, callback_data: dict[str, str]):
+class BanUserAlertResponse(BaseResponse):
+    def __init__(self, query: CallbackQuery, user: schemas.User,
+                 callback_data: dict[str, str]):
         self.__query = query
         self.__user = user
         self.__keyboard = common_keybords.ConfirmationKeyboard(
-            callback_factories.UserCallbackFactory(), **callback_data
+            callback_factory=UserCallbackFactory(),
+            **callback_data,
         )
 
     async def _send_response(self):
@@ -94,71 +144,92 @@ class BanUserAlertResponse(base.BaseResponse):
         )
 
 
-class UnbanUserAlertResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User, callback_data: dict[str, str]):
+class UnbanUserAlertResponse(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            user: schemas.User,
+            callback_data: dict[str, str],
+    ):
         self.__query = query
         self.__user = user
         self.__keyboard = common_keybords.ConfirmationKeyboard(
-            callback_factories.UserCallbackFactory(), **callback_data
+            callback_factory=UserCallbackFactory(),
+            **callback_data,
         )
 
     async def _send_response(self):
+        username = self.__user.username or 'user'
         await self.__query.answer()
         await self.__query.message.edit_text(
-            f'Are you sure you want to unban '
-            f'{self.__user.username if self.__user.username is not None else "user"} '
+            f'Are you sure you want to unban {username} '
             f'with {self.__user.telegram_id}?',
             reply_markup=self.__keyboard
         )
 
 
-class DeleteUserAlert(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User, callback_data: dict[str, str]):
+class DeleteUserAlert(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            user: schemas.User,
+            callback_data: dict[str, str],
+    ):
         self.__query = query
         self.__user = user
         self.__keyboard = common_keybords.ConfirmationKeyboard(
-            callback_factories.UserCallbackFactory(), **callback_data
+            callback_factory=UserCallbackFactory(),
+            **callback_data,
         )
 
     async def _send_response(self):
         text = (
-            f'This user has ${self.__user.balance} left. Are you sure you want to delete this user?'
+            f'This user has ${self.__user.balance} left.'
+            f' Are you sure you want to delete this user?'
         )
         await self.__query.answer()
         await self.__query.message.edit_text(text, reply_markup=self.__keyboard)
 
 
-class SuccessUserRemovalResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery,  user: schemas.User):
+class SuccessUserRemovalResponse(BaseResponse):
+    def __init__(self, query: CallbackQuery, user: schemas.User):
         self.__user = user
         self.__query = query
 
-    async def _send_response(self) -> aiogram.types.Message:
-        text = (f'✅ Deleted {self.__user.username or "user"} with {self.__user.telegram_id}'
-                f' and previous balance of {self.__user.balance}')
+    async def _send_response(self) -> Message:
+        username = self.__user.username or 'user'
+        text = (
+            f'✅ Deleted {username} with {self.__user.telegram_id}'
+            f' and previous balance of {self.__user.balance}'
+        )
         await self.__query.answer()
         return await self.__query.message.edit_text(text=text)
 
 
-class EditBalanceAlertResponse(base.BaseResponse):
-    def __init__(self, message: aiogram.types.Message, user: schemas.User,
+class EditBalanceAlertResponse(BaseResponse):
+    def __init__(self, message: Message, user: schemas.User,
                  new_balance: str, callback_data: dict[str, str]):
         self.__message = message
         self.__user = user
         self.__new_balance = new_balance
         callback_data['balance'] = new_balance
         self.__keyboard = common_keybords.ConfirmationKeyboard(
-            callback_factories.EditUserBalanceCallbackFactory(), **callback_data
+            callback_factory=EditUserBalanceCallbackFactory(),
+            **callback_data,
         )
 
     async def _send_response(self):
-        text = (f'Are you sure you want to change the balance of {self.__user.username or "user"}'
-                f' with {self.__user.telegram_id} from ${self.__user.balance} to ${self.__new_balance}?')
+        username = self.__user.username or 'user'
+        text = (
+            f'Are you sure you want to change the balance of {username}'
+            f' with {self.__user.telegram_id}'
+            f' from ${self.__user.balance} to ${self.__new_balance}?'
+        )
         await self.__message.answer(text, reply_markup=self.__keyboard)
 
 
-class NewBalanceResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery):
+class NewBalanceResponse(BaseResponse):
+    def __init__(self, query: CallbackQuery):
         self.__query = query
 
     async def _send_response(self):
@@ -167,19 +238,20 @@ class NewBalanceResponse(base.BaseResponse):
         await self.__query.message.edit_text(text)
 
 
-class IncorrectBalanceResponse(base.BaseResponse):
-    def __init__(self, message: aiogram.types.Message):
+class IncorrectBalanceResponse(BaseResponse):
+    def __init__(self, message: Message):
         self.__message = message
 
     async def _send_response(self):
         await self.__message.answer('❌ Incorrect balance!')
 
 
-class BalanceEditingReasonResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, callback_data: dict[str, str]):
+class BalanceEditingReasonResponse(BaseResponse):
+    def __init__(self, query: CallbackQuery, callback_data: dict[str, str]):
         self.__query = query
         self.__keyboard = users_keyboard.BalanceEditingReasonsKeyboard(
-            int(callback_data['user_id']), float(callback_data['balance'])
+            user_id=int(callback_data['user_id']),
+            balance=float(callback_data['balance']),
         )
 
     async def _send_response(self):
@@ -188,50 +260,76 @@ class BalanceEditingReasonResponse(base.BaseResponse):
         await self.__query.message.edit_text(text, reply_markup=self.__keyboard)
 
 
-class SuccessBalanceEditing(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User, new_balance: float, reason: str):
+class SuccessBalanceEditing(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            user: schemas.User,
+            new_balance: float,
+            reason: str,
+    ):
         self.__query = query
         self.__user = user
         self.__new_balance = new_balance
         self.__reason = reason
 
     async def _send_response(self):
+        # TODO Refactor it 😱
+        now = get_now_datetime()
         text = (
                 f'Changed balance of {self.__user.username or "user"}'
                 f' with {self.__user.telegram_id} from ${self.__user.balance} to ${self.__new_balance}\n'
-                f'<code>Date: {datetime.date.today()}{" " * 150}' +
-                (f'Username: {self.__user.username}{" " * 150}' if self.__user.username is not None else '') +
+                f'<code>Date: {now:%m/%d/%Y}{" " * 150}' +
+                (
+                    f'Username: {self.__user.username}{" " * 150}' if self.__user.username is not None else '') +
                 f'ID: {self.__user.telegram_id}{" " * 150}'
                 f'Previous balance: {self.__user.balance}{" " * 150}'
                 f'New Balance: {self.__new_balance}{" " * 150}'
                 f'Reason: {self.__reason}</code>'
         )
         await self.__query.answer()
-        await self.__query.message.answer(text, parse_mode='HTML')
+        await self.__query.message.answer(
+            text=text,
+            parse_mode=ParseMode.HTML,
+        )
 
 
-class TopUpBalanceAlertResponse(base.BaseResponse):
-    def __init__(self, message: aiogram.types.Message, user: schemas.User,
-                 balance: str, callback_data: dict[str, str]):
+class TopUpBalanceAlertResponse(BaseResponse):
+    def __init__(
+            self,
+            message: Message,
+            user: schemas.User,
+            balance: str,
+            callback_data: dict[str, str],
+    ):
         self.__message = message
         self.__user = user
         self.__balance = balance
         callback_data['balance_delta'] = balance
         self.__keyboard = common_keybords.ConfirmationKeyboard(
-            callback_factories.TopUpUserBalanceCallbackFactory(), **callback_data
+            callback_factory=TopUpUserBalanceCallbackFactory(),
+            **callback_data,
         )
 
     async def _send_response(self):
-        text = (f'Are you sure you want to top-up the balance of {self.__user.username or "user"}'
-                f' with {self.__user.telegram_id} for ${self.__balance}?')
+        username = self.__user.username or 'user'
+        text = (
+            f'Are you sure you want to top-up the balance of {username}'
+            f' with {self.__user.telegram_id} for ${self.__balance}?'
+        )
         await self.__message.answer(text, reply_markup=self.__keyboard)
 
 
-class BalanceRefillMethodResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, callback_data: dict[str, str]):
+class BalanceRefillMethodResponse(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            callback_data: dict[str, str],
+    ):
         self.__query = query
         self.__keyboard = users_keyboard.BalanceRefillMethodsKeyboard(
-            int(callback_data['user_id']), float(callback_data['balance_delta'])
+            user_id=int(callback_data['user_id']),
+            balance=float(callback_data['balance_delta']),
         )
 
     async def _send_response(self):
@@ -240,23 +338,32 @@ class BalanceRefillMethodResponse(base.BaseResponse):
         await self.__query.message.edit_text(text, reply_markup=self.__keyboard)
 
 
-class SuccessBalanceRefillResponse(base.BaseResponse):
-    def __init__(self, query: aiogram.types.CallbackQuery, user: schemas.User, balance_delta: float, method: str):
+class SuccessBalanceRefillResponse(BaseResponse):
+    def __init__(
+            self,
+            query: CallbackQuery,
+            user: schemas.User,
+            balance_delta: float,
+            method: str,
+    ):
         self.__query = query
         self.__user = user
         self.__balance_delta = balance_delta
         self.__method = method
 
     async def _send_response(self):
+        # TODO Refactor it too 😱
+        now = get_now_datetime()
         text = (
-            f'Topped-up {self.__user.username or "user"} '
-            f'with {self.__user.telegram_id} for ${self.__balance_delta}\n'
-            f'<code>Date: {datetime.date.today()}{150 * " "}' +
-            (f'Username: {self.__user.username}{150 * " "}' if self.__user.username is not None else '') +
-            f'ID: {self.__user.telegram_id}{150 * " "}'
-            f'Topped Up amount: {self.__balance_delta}{150 * " "}'
-            f'Total Balance: {self.__user.balance}{150 * " "}'
-            f'Method of payment: {self.__method}</code>'
+                f'Topped-up {self.__user.username or "user"} '
+                f'with {self.__user.telegram_id} for ${self.__balance_delta}\n'
+                f'<code>Date: {now:%m/%d/%Y}{150 * " "}' +
+                (
+                    f'Username: {self.__user.username}{150 * " "}' if self.__user.username is not None else '') +
+                f'ID: {self.__user.telegram_id}{150 * " "}'
+                f'Topped Up amount: {self.__balance_delta}{150 * " "}'
+                f'Total Balance: {self.__user.balance}{150 * " "}'
+                f'Method of payment: {self.__method}</code>'
         )
         await self.__query.answer()
         await self.__query.message.answer(text)
