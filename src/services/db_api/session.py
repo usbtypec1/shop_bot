b78528ec0639748aa9ae00a7aac0fd42d420af65
Eldos <eldos.baktybekov@gmail.com>
2023-05-19 16:06:22 +0600

    :card_file_box: Add new session factory

--- src/services/db_api/session.py
+++ src/services/db_api/session.py
@@ -1,16 +1,21 @@
 import contextlib
-import typing
 
-import sqlalchemy.orm
-from sqlalchemy import orm
+from sqlalchemy.orm import sessionmaker, Session
 
-from services.db_api import engine
+from services.db_api.engine import engine
+
+__all__ = (
+    'session_factory',
+)
 
 
-RawSession = sqlalchemy.orm.sessionmaker(bind=engine.engine)
+# TODO Delete it
+RawSession = sessionmaker(bind=engine.engine)
+
+session_factory = sessionmaker(bind=engine, expire_on_commit=False)
 
 
 @contextlib.contextmanager
-def create_session() -> typing.Generator[orm.Session, None, None]:
+def create_session() -> Session:
     with RawSession() as session, session.begin():
         yield session
