8a4f8f5841a2e87a8ff5686391c623fae404ada2
bestmgmt <129497629+bestmgmt@users.noreply.github.com>
2023-05-20 03:50:18 -0400

    Update profile.py
    - Added the new self.__balance that can query the database using a new query called get_user_balance

--- src/handlers/users/profile.py
+++ src/handlers/users/profile.py
@@ -9,15 +9,29 @@ from services.db_api import queries
 import exceptions
 
 
-@dp.message_handler(filters.Text('📱 Profile'))
+# @dp.message_handler(filters.Text('📱 Profile'))
+# async def profile(message: aiogram.types.Message):
+#     with db_api.create_session() as session:
+#         user = queries.get_user(session, telegram_id=message.from_user.id)
+#         if user is None:
+#             raise exceptions.UserNotInDatabase
+#         queries.get_purchases(session, message.from_user.id)
+#         await responses.profile.ProfileResponse(
+#             message, user.telegram_id, user.username,
+#             queries.count_user_purchases(session, user.id),
+#             queries.get_user_orders_amount(session, user.id),
+#             queries.get_purchases(session, user.id, limit=10)
+#         )
+
+ @dp.message_handler(filters.Text('📱 Profile'))
 async def profile(message: aiogram.types.Message):
     with db_api.create_session() as session:
         user = queries.get_user(session, telegram_id=message.from_user.id)
         if user is None:
             raise exceptions.UserNotInDatabase
-        queries.get_purchases(session, message.from_user.id)
+        user_balance = queries.get_user_balance(session, user.id)  # liked to a new query get_user_balance()
         await responses.profile.ProfileResponse(
-            message, user.telegram_id, user.username,
+            message, user.telegram_id, user.username, user_balance,
             queries.count_user_purchases(session, user.id),
             queries.get_user_orders_amount(session, user.id),
             queries.get_purchases(session, user.id, limit=10)
