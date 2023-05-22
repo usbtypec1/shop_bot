c1d43de0c9f2935345779db3356f989e195ea0ce
Eldos <eldos.baktybekov@gmail.com>
2023-05-22 23:14:40 +0600

    :technologist: Add special methods for ticket number display

--- src/models/support_tickets.py
+++ src/models/support_tickets.py
@@ -17,3 +17,23 @@ class SupportTicket:
     issue: str
     answer: str | None
     status: SupportTicketStatus
+
+    @property
+    def id_display(self) -> int:
+        """
+        Please use this property to display the ID in the UI.
+        It is needed to enable visibility of a large number of requests.
+        """
+        return self.to_display_id(self.id)
+
+    @staticmethod
+    def to_display_id(value: int) -> int:
+        """Convert internal ID to the display ID."""
+        return value + 10015
+
+    @staticmethod
+    def to_internal_id(value: int) -> int:
+        """
+        Convert display ID to the ID which is stored in database.
+        """
+        return value - 10015
