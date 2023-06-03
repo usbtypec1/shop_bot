5ecac5119d9d8b72ba2d3b2d6c93ca0c5462e646
Eldos <eldos.baktybekov@gmail.com>
2023-06-03 14:00:57 +0600

    :sparkles: Add methods in user repository

--- src/repositories/database/users.py
+++ src/repositories/database/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func, delete
+from sqlalchemy import select, func, delete, update
 
 import exceptions
 import models
@@ -9,6 +9,19 @@ __all__ = ('UserRepository',)
 
 
 class UserRepository(BaseRepository):
+
+    def get_by_id(self, user_id: int) -> models.User:
+        with self._session_factory() as session:
+            result = session.get(User, user_id)
+        if result is None:
+            raise exceptions.UserNotInDatabase
+        return models.User(
+            id=result.id,
+            telegram_id=result.telegram_id,
+            username=result.username,
+            balance=result.balance,
+            is_banned=result.is_banned,
+        )
 
     def get_by_telegram_id(self, telegram: int) -> models.User:
         statement = select(User).where(User.telegram_id == telegram)
@@ -54,3 +67,31 @@ class UserRepository(BaseRepository):
         with self._session_factory() as session:
             result = session.execute(statement).first()
         return result[0]
+
+    def get_total_count(self) -> int:
+        statement = select(func.count(User.id))
+        with self._session_factory() as session:
+            result = session.execute(statement).first()
+        return result[0]
+
+    def ban_by_id(self, user_id: int) -> bool:
+        statement = (
+            update(User)
+            .where(User.id == user_id)
+            .values(is_banned=True)
+        )
+        with self._session_factory() as session:
+            with session.begin():
+                result = session.execute(statement)
+        return bool(result.rowcount)
+
+    def unban_by_id(self, user_id: int) -> bool:
+        statement = (
+            update(User)
+            .where(User.id == user_id)
+            .values(is_banned=False)
+        )
+        with self._session_factory() as session:
+            with session.begin():
+                result = session.execute(statement)
+        return bool(result.rowcount)
