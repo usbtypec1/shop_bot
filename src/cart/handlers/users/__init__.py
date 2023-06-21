39641afed5a27d5682a287fc288232be12e9648e
Eldos <eldos.baktybekov@gmail.com>
2023-06-21 20:14:13 +0600

    :sparkles: Add functionality to add product to shopping cart

--- src/cart/handlers/users/__init__.py
+++ src/cart/handlers/users/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Dispatcher
 
-from . import menu
+from . import menu, products
 
 __all__ = ('register_handlers',)
 
 
 def register_handlers(dispatcher: Dispatcher) -> None:
     menu.register_handlers(dispatcher)
+    products.register_handlers(dispatcher)
