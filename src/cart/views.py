ffaab3775be6373968a9176c8c59f1192b9af1b4
Eldos <eldos.baktybekov@gmail.com>
2023-06-21 20:09:36 +0600

    :lipstick: Add ProductQuantityOutOfRangeWarningView

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,11 +1,20 @@
 from collections.abc import Iterable
+from typing import Protocol
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from cart.models import CartProduct
 from common.views import View
 
-__all__ = ('UserShoppingCartView',)
+__all__ = (
+    'UserShoppingCartView',
+    'ProductQuantityOutOfRangeWarningView',
+)
+
+
+class HasOptionalMinAndMaxOrderQuantity(Protocol):
+    min_order_quantity: int | None
+    max_order_quantity: int | None
 
 
 class UserShoppingCartView(View):
@@ -26,7 +35,7 @@ class UserShoppingCartView(View):
 
         return (
             f'Total items: {cart_products_total_count}'
-            f' / Total Amount: {cart_products_total_cost:.2f}'
+            f' / Total Amount: ${cart_products_total_cost:.2f}'
         )
 
     def get_reply_markup(self) -> InlineKeyboardMarkup:
@@ -84,3 +93,34 @@ class UserShoppingCartView(View):
         )
 
         return markup
+
+
+class ProductQuantityOutOfRangeWarningView(View):
+
+    def __init__(self, product: HasOptionalMinAndMaxOrderQuantity):
+        self.__product = product
+
+    def get_text(self) -> str:
+        has_min_order_quantity = self.__product.min_order_quantity is not None
+        has_max_order_quantity = self.__product.max_order_quantity is not None
+
+        if has_min_order_quantity and has_max_order_quantity:
+            return (
+                'You can\'t order less than'
+                f' {self.__product.min_order_quantity}'
+                f' or more than {self.__product.max_order_quantity}'
+            )
+        if has_min_order_quantity:
+            return (
+                'You can\'t order less than'
+                f' {self.__product.min_order_quantity}'
+            )
+        if has_max_order_quantity:
+            return (
+                'You can\'t order more than'
+                f' {self.__product.max_order_quantity}'
+            )
+        raise ValueError(
+            'Neither `min_order_quantity` nor `max_order_quantity`'
+            ' has been specified for this product'
+        )
