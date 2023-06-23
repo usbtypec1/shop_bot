51bdaf138a8b459c8670d301f115d196ff2b19ab
Eldos <eldos.baktybekov@gmail.com>
2023-06-24 00:49:31 +0600

    :recycle: Count user's orders by ID instead of Telegram ID

--- src/sales/repositories.py
+++ src/sales/repositories.py
@@ -1,19 +1,15 @@
 from sqlalchemy import select, func
 
 from common.repositories import BaseRepository
-from database.schemas import User, Sale
+from database.schemas import Sale
 
 __all__ = ('SaleRepository',)
 
 
 class SaleRepository(BaseRepository):
 
-    def count_by_user_telegram_id(self, user_telegram_id: int) -> int:
-        statement = (
-            select(func.count())
-            .join(User, Sale.user_id == User.id)
-            .where(User.telegram_id == user_telegram_id)
-        )
+    def count_by_user_id(self, user_id: int) -> int:
+        statement = select(func.count()).where(Sale.user_id == user_id)
         with self._session_factory() as session:
             row = session.execute(statement).first()
 
