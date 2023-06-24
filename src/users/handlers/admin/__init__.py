dc11abf96f44d40b194a7b16ff7091ff243ba809
Eldos <eldos.baktybekov@gmail.com>
2023-06-24 20:30:40 +0600

    :recycle: Move user search handlers into separated file

--- src/users/handlers/admin/__init__.py
+++ src/users/handlers/admin/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 
-from . import list, detail, delete
+from . import list, detail, delete, search
 
 __all__ = ('register_handlers',)
 
@@ -9,3 +9,4 @@ def register_handlers(dispatcher: Dispatcher) -> None:
     list.register_handlers(dispatcher)
     detail.register_handlers(dispatcher)
     delete.register_handlers(dispatcher)
+    search.register_handlers(dispatcher)
--- src/users/handlers/admin_old.py
+++ src/users/handlers/admin_old.py
@@ -41,41 +41,6 @@ async def user_not_in_db_error(
     return True
 
 
-async def on_start_search_users_flow(
-        callback_query: CallbackQuery,
-        state: FSMContext,
-) -> None:
-    await state.finish()
-    await callback_query.message.answer('🆔 Enter usernames or ids')
-    await SearchUsersStates.waiting_identifiers.set()
-
-
-async def on_users_identifiers_for_search_input(
-        message: Message,
-        state: FSMContext,
-        user_repository: UserRepository,
-):
-    await state.finish()
-    page, page_size = 0, 10
-    users_identifiers = parse_users_identifiers_for_search(message.text)
-    users = user_repository.get_by_usernames_and_ids(
-        usernames=users_identifiers.usernames,
-        user_ids=users_identifiers.user_ids,
-        limit=page_size,
-        offset=page * page_size,
-    )
-    total_balance = calculate_total_balance(users)
-    sent_message = await message.answer(
-        f'🔡 Found users with these usernames and ids: {message.text}'
-    )
-    view = UserListView(
-        users=users,
-        total_balance=float(total_balance),
-        users_filter=sent_message.message_id,
-        page=page,
-        page_size=page_size,
-    )
-    await answer_view(message=message, view=view)
 
 
 async def ban_user(query: CallbackQuery, callback_data: dict[str: str]) -> None:
@@ -253,20 +218,6 @@ def register_handlers(dispatcher: Dispatcher) -> None:
     dispatcher.register_errors_handler(
         user_not_in_db_error,
         exception=UserNotInDatabase,
-    )
-    dispatcher.register_callback_query_handler(
-        on_start_search_users_flow,
-        AdminFilter(),
-        Text('search-users'),
-        chat_type=ChatType.PRIVATE,
-        state='*',
-    )
-    dispatcher.register_message_handler(
-        on_users_identifiers_for_search_input,
-        AdminFilter(),
-        content_types=ContentType.TEXT,
-        chat_type=ChatType.PRIVATE,
-        state=SearchUsersStates.waiting_identifiers,
     )
     dispatcher.register_callback_query_handler(
         ban_user,
