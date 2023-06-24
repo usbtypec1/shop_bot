833ec4a712f49b7faa72d3950f8030067902d0fa
Eldos <eldos.baktybekov@gmail.com>
2023-06-24 23:32:57 +0600

    :recycle: Refactor user management update handlers location

--- src/users/handlers/admin/update/balance/__init__.py
+++ src/users/handlers/admin/update/balance/__init__.py
@@ -1,10 +1,9 @@
 from aiogram import Dispatcher
 
-from . import is_banned, balance
+from . import top_up
 
 __all__ = ('register_handlers',)
 
 
 def register_handlers(dispatcher: Dispatcher) -> None:
-    is_banned.register_handlers(dispatcher)
-    balance.register_handlers(dispatcher)
+    top_up.register_handlers(dispatcher)
