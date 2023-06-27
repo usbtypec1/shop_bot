e42de975328a873ec076adb7d1cbfdc134a1acc1
Eldos <eldos.baktybekov@gmail.com>
2023-06-27 17:24:44 +0600

    :lipstick: Add top up bonus views

--- src/top_up_bonuses/views.py
+++ src/top_up_bonuses/views.py
@@ -1,4 +1,6 @@
 from collections.abc import Iterable
+from datetime import datetime
+from decimal import Decimal
 
 from aiogram.types import (
     InlineKeyboardMarkup,
@@ -14,6 +16,8 @@ from top_up_bonuses.models import TopUpBonus
 __all__ = (
     'TopUpBonusListView',
     'TopUpBonusMenuView',
+    'TopUpBonusCreateAskForConfirmationView',
+    'TopUpBonusCreateReceiptView',
 )
 
 
@@ -64,3 +68,90 @@ class TopUpBonusMenuView(View):
             ],
         ],
     )
+
+
+class TopUpBonusCreateAskForConfirmationView(View):
+
+    def __init__(
+            self,
+            *,
+            minimum_amount: Decimal,
+            bonus_percentage: int,
+            starts_at: datetime | None,
+            expires_at: datetime | None,
+    ):
+        self.__minimum_amount = minimum_amount
+        self.__bonus_percentage = bonus_percentage
+        self.__starts_at = starts_at
+        self.__expires_at = expires_at
+
+    def get_text(self) -> str:
+        if self.__starts_at is None:
+            starts_at = 'now'
+        else:
+            starts_at = f'{self.__starts_at:%m/%d/%Y %H:%M}'
+
+        if self.__expires_at is None:
+            expires_at = 'infinite'
+        else:
+            expires_at = f'{self.__expires_at:%m/%d/%Y %H:%M}'
+
+        return (
+            f'Are you sure you want to apply {self.__bonus_percentage}%'
+            f' bonus to any amount above ${self.__minimum_amount:.2f}'
+            f' which begins in {starts_at}'
+            f' and finishes in {expires_at}'
+        )
+
+    def get_reply_markup(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text='Yes',
+                        callback_data='top-up-bonus-create-confirm',
+                    ),
+                    InlineKeyboardButton(
+                        text='No',
+                        callback_data='show-top-up-bonus-list',
+                    ),
+                ],
+            ],
+        )
+
+
+class TopUpBonusCreateReceiptView(View):
+
+    def __init__(
+            self,
+            *,
+            minimum_amount: Decimal,
+            bonus_percentage: int,
+            starts_at: datetime | None,
+            expires_at: datetime | None,
+    ):
+        self.__minimum_amount = minimum_amount
+        self.__bonus_percentage = bonus_percentage
+        self.__starts_at = starts_at
+        self.__expires_at = expires_at
+
+    def get_text(self) -> str:
+        if self.__starts_at is None:
+            starts_at = 'now'
+        else:
+            starts_at = f'{self.__starts_at:%m/%d/%Y %H:%M}'
+
+        if self.__expires_at is None:
+            expires_at = 'infinite'
+        else:
+            expires_at = f'{self.__expires_at:%m/%d/%Y %H:%M}'
+
+        return (
+            '<code>'
+            'Minimum amount to activate this top up bonus:'
+            f' ${self.__minimum_amount:.2f}\n'
+            f'Amount of bonus: {self.__bonus_percentage}%\n'
+            f'Start Date: {starts_at}\n'
+            f'Finish Date: {expires_at}\n'
+            '</code>'
+        )
