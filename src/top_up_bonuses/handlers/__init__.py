5f6a056581f87e6b8c29da8b63fb661447651bd7
Eldos <eldos.baktybekov@gmail.com>
2023-06-28 01:35:47 +0600

    :sparkles: Add top up bonus detail/delete handlers

--- src/top_up_bonuses/handlers/__init__.py
+++ src/top_up_bonuses/handlers/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 
-from . import list, menu, create
+from . import list, menu, create, detail, delete
 
 __all__ = ('register_handlers',)
 
@@ -9,3 +9,5 @@ def register_handlers(dispatcher: Dispatcher) -> None:
     list.register_handlers(dispatcher)
     menu.register_handlers(dispatcher)
     create.register_handlers(dispatcher)
+    detail.register_handlers(dispatcher)
+    delete.register_handlers(dispatcher)
