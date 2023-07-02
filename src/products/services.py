35f750c4774aba00e5cca9ca654669a3294e0996
Eldos <eldos.baktybekov@gmail.com>
2023-07-02 16:52:49 +0600

    :fire: Remove unused code

--- src/products/services.py
+++ src/products/services.py
@@ -1,7 +1,7 @@
 import contextlib
 import pathlib
 import shutil
-from collections.abc import Iterable, Callable
+from collections.abc import Iterable
 from uuid import UUID
 
 import structlog
@@ -11,8 +11,6 @@ from aiogram.types import (
     InputMediaPhoto,
     InputMediaVideo,
     InputMedia,
-    InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
 )
 from aiogram.utils.exceptions import TelegramAPIError
 
@@ -90,83 +88,6 @@ async def answer_view_with_media(
                 logger.error('Could not send media group')
 
     return await answer_view(message=message, view=view)
-
-
-async def answer_media_with_text(
-        message: Message,
-        base_path: pathlib.Path,
-        product: Product,
-        caption: str | None = None,
-        reply_markup: InlineKeyboardMarkup | ReplyKeyboardMarkup | None = None,
-):
-    product.media
-    file_path = base_path / product.picture
-    if file_path.name.endswith('.gif.mp4'):
-        answer = message.answer_animation
-    else:
-        file_extension_strategies = {
-            '.jpg': message.answer_photo,
-            '.mp4': message.answer_video,
-        }
-        answer = file_extension_strategies[file_path.suffix]
-    with file_path.open(mode='rb') as file_io:
-        await answer(file_io, caption=caption, reply_markup=reply_markup)
-
-
-async def answer_medias(
-        message: Message,
-        base_path: pathlib.Path,
-        product: Product,
-):
-    animation_file_paths = build_file_paths(
-        base_path=base_path,
-        file_names=product.animation_file_names,
-    )
-    for animation_file_path in animation_file_paths:
-        with animation_file_path.open(mode='rb') as file_io:
-            await message.answer_animation(file_io)
-
-    photo_and_video_file_paths = build_file_paths(
-        base_path=base_path,
-        file_names=product.photo_and_video_file_names,
-    )
-
-    if photo_and_video_file_paths:
-
-        with contextlib.ExitStack() as exit_stack:
-            media_group = MediaGroup()
-            for file_path in photo_and_video_file_paths:
-                attach_to_media_group_and_exit_stack(
-                    file_path=file_path,
-                    media_group=media_group,
-                    exit_stack=exit_stack,
-                )
-            await message.answer_media_group(media_group)
-
-
-def attach_to_media_group_and_exit_stack(
-        *,
-        file_path: pathlib.Path,
-        media_group: MediaGroup,
-        exit_stack: contextlib.ExitStack,
-) -> None:
-    file_extension_strategies: (
-        dict[str, tuple[type[InputMedia], Callable[[InputMedia], None]]]
-    ) = {
-        '.jpg': (InputMediaPhoto, media_group.attach_photo),
-        '.mp4': (InputMediaVideo, media_group.attach_video),
-    }
-    try:
-        input_media_type, attach_media = (
-            file_extension_strategies[file_path.suffix]
-        )
-    except KeyError:
-        raise ValueError(f'Invalid file extension: {file_path.suffix}')
-
-    # ensure file IO will be closed
-    file_io = exit_stack.enter_context(file_path.open(mode='rb'))
-
-    attach_media(input_media_type(file_io))
 
 
 def batch_move_files(
