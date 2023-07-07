7487785437929739cba4085d5ba1b0f4ab3d8af4
Eldos <eldos.baktybekov@gmail.com>
2023-07-07 15:13:21 +0600

    :sparkles: Display discounted price

--- src/products/handlers/users/detail.py
+++ src/products/handlers/users/detail.py
@@ -3,25 +3,30 @@ from aiogram.types import CallbackQuery
 
 import config
 from products.callback_data import UserProductDetailCallbackData
-
-__all__ = ('register_handlers',)
-
 from products.repositories import ProductRepository
 from products.services import answer_view_with_media
 from products.views import UserProductDetailView
+from users.repositories import UserRepository
+
+__all__ = ('register_handlers',)
 
 
 async def on_show_product_detail(
         callback_query: CallbackQuery,
         callback_data: dict,
         product_repository: ProductRepository,
+        user_repository: UserRepository,
 ) -> None:
+    user = user_repository.get_by_telegram_id(callback_query.from_user.id)
     product_id: int = callback_data['product_id']
     product = product_repository.get_by_id(product_id)
     if not product.can_be_purchased:
         await callback_query.answer('Coming soon...', show_alert=True)
         return
-    view = UserProductDetailView(product)
+    view = UserProductDetailView(
+        product=product,
+        permanent_discount=user.permanent_discount,
+    )
     await answer_view_with_media(
         view=view,
         message=callback_query.message,
--- src/products/views/users.py
+++ src/products/views/users.py
@@ -1,24 +1,39 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from common.services import render_money
 from common.views import View
 from products.callback_data import (
     UserProductAddToCartCallbackData,
     UserProductBuyCallbackData
 )
 from products.models import Product
+from users.services import calculate_discounted_price
 
 __all__ = ('UserProductDetailView',)
 
 
 class UserProductDetailView(View):
 
-    def __init__(self, product: Product):
+    def __init__(self, *, product: Product, permanent_discount: int):
         self.__product = product
+        self.__permanent_discount = permanent_discount
 
     def get_text(self) -> str:
         has_stocks = self.__product.quantity > 0
         has_single_stock = self.__product.quantity == 1
         is_stocks_displayed = self.__product.max_displayed_stock_count != 1
+
+        if self.__permanent_discount == 0:
+            price_to_display = f'${render_money(self.__product.price)}'
+        else:
+            discounted_price = calculate_discounted_price(
+                original_price=self.__product.price,
+                discount_percentage=self.__permanent_discount
+            )
+            price_to_display = (
+                f'<s>${render_money(self.__product.price)}</s>'
+                f' ${render_money(discounted_price)}'
+            )
 
         if not is_stocks_displayed and has_stocks:
             stocks = 'In Stock'
@@ -33,7 +48,7 @@ class UserProductDetailView(View):
         lines = [
             f'📓 Name: {self.__product.name}',
             f'📋 Description:\n{self.__product.description}',
-            f'💳 Price: ${self.__product.price:.2f}',
+            f'💳 Price: {price_to_display}',
             f'📦 Available to purchase: {stocks}'
         ]
         return '\n'.join(lines)
